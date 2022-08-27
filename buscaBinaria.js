df93e6afe7620de33f141b0c50b71391c735eb6d
Nicholas( ) <59513778+DoctorRedacted@users.noreply.github.com>
2022-08-27 05:16:41 -0300

    else_if_adicionado

--- buscaBinaria.js
+++ buscaBinaria.js
@@ -1,22 +1,26 @@
 function binario(number, n) {
     let max = number.length-1;
     let min = 0;
-    let mid = Math.floor((max + min) /2);
+    let mid = Math.floor((max + min) / 2);
     let cont = 1;
     
-    for (let i = 0; i <= Math.floor(number.length/2); i++) {
+    for (let i = 0; i <= Math.log2(number.length / 2); i++) {
         console.log(cont++)
         if (number[mid] == n) {
             console.log(`O item ${n} está na posição ${mid}`);
             break;
         }
+        else if (number[mid + 1] == n) {
+            console.log(`O item ${n} está na posição ${mid+1}`);
+            break;
+        }
         else if (number[mid] < n) {
             min = mid+1;
-            mid = Math.floor((min + max) /2);
+            mid = Math.floor((min+max)/2);
         }
         else if (number[mid] > n) {
             max = mid-1;
-            mid = Math.floor((min + max) /2);
+            mid = Math.floor((min+max)/2);
         };
     };
 };
